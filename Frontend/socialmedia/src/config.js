16183558de2c15a2a5f4bdca4869c6edb66dd137
Shrey Chamoli <shreychamoli83@gmail.com>
2025-05-30 12:56:22 +0530

    Commit changes

--- Frontend/socialmedia/src/config.js
+++ Frontend/socialmedia/src/config.js
@@ -1,2 +1,2 @@
-const REACT_APP_BASE_URL = 'https://connected-social-media-platform-bac.vercel.app'
+const REACT_APP_BASE_URL = 'https://connected-social-media-platform-backend1.onrender.com'
 export default REACT_APP_BASE_URL
