63c607b07fe066fede6bb952acda2f4031fc560f
Shrey Chamoli <shreychamoli83@gmail.com>
2025-05-30 11:49:31 +0530

    Changed Backend url

--- Frontend/socialmedia/src/config.js
+++ Frontend/socialmedia/src/config.js
@@ -1,2 +1,2 @@
-const REACT_APP_BASE_URL = 'https://connected-social-media-platform-9of.vercel.app'
+const REACT_APP_BASE_URL = 'https://connected-social-media-platform-bac.vercel.app'
 export default REACT_APP_BASE_URL
