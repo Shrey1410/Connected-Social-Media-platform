602daebc25ae92a0f4500782c0d94ee2bc83b0b6
Shrey Chamoli <shreychamoli83@gmail.com>
2025-05-30 17:38:24 +0530

    commit changes

--- Frontend/socialmedia/src/components/Comments.jsx
+++ Frontend/socialmedia/src/components/Comments.jsx
@@ -73,7 +73,7 @@ const Comments = (props) => {
       alt="User Profile"
     />
   </div>
-  {post.createdBy?.Online && <span className="w-2 h-2 bg-green-500 rounded-full inline-block"></span>}
+  {comment.user_id.Online && <span className="w-2 h-2 bg-green-500 rounded-full inline-block"></span>}
   {/* Comment Bubble */}
   <div className='bg-gray-100 rounded-2xl px-4 py-2 w-full'>
       <p className='text-sm break-words whitespace-pre-wrap' key={comment._id}>
