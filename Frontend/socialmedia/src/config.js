eec0de1e41a6982942d0f9038ba6262d8a77721f
Shrey Chamoli <shreychamoli83@gmail.com>
2025-05-31 19:38:21 +0530

    Correction in Last commit

--- Frontend/socialmedia/src/config.js
+++ Frontend/socialmedia/src/config.js
@@ -1,3 +1,3 @@
-// const REACT_APP_BASE_URL = 'https://connected-social-media-platform.onrender.com'
-const REACT_APP_BASE_URL = 'http://localhost:8000' 
+const REACT_APP_BASE_URL = 'https://connected-social-media-platform.onrender.com'
+// const REACT_APP_BASE_URL = 'http://localhost:8000' 
 export default REACT_APP_BASE_URL
